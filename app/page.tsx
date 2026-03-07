a26d41dc337291ccec939a601fa1c8bde7776162
irangeluchoa-jpg <irangeluchoa@gmail.com>
2026-03-07 17:29:50 -0300

    mega update

--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { MainMenu } from "@/components/menu/main-menu"
-
-export default function HomePage() {
-  return <MainMenu />
-}
--- app/songs/page.tsx
+++
@@ -1,5 +0,0 @@
-import { SongSelect } from "@/components/menu/song-select"
-
-export default function SongsPage() {
-  return <SongSelect />
-}
